f37c8c33e8b7cafe759efe8498f7b13112c93a6d
Johannes Hofmann <johannes_hofmann@me.com>
2019-01-29 10:54:14 +0100

    adds test block to test for empty configuration

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,44 +1,71 @@
 const ServerlessPluginInvokeLocalEnvironment = require('./index');
 
-const configKey = 'KEY';
-const configValue = 'value';
-const configString = `${configKey}=${configValue}`;
-
-const serverlessMock = {
-  service: {
-    custom: {
-      invokeLocalEnvironment: configString,
-    },
-    getFunction: jest.fn(() => ({
-      environment: {},
-    })),
-  },
-  cli: {
-    log: jest.fn(),
-  },
-};
-const optionsMock = {
-  function: 'testFunction',
-};
-
 let plugin;
 
-beforeEach(() => {
-  plugin = new ServerlessPluginInvokeLocalEnvironment(serverlessMock, optionsMock);
+describe('configuration provided', () => {
+  const configKey = 'KEY';
+  const configValue = 'value';
+  const configString = `${configKey}=${configValue}`;
+
+  const serverlessMock = {
+    service: {
+      custom: {
+        invokeLocalEnvironment: configString,
+      },
+      getFunction: jest.fn(() => ({
+        environment: {},
+      })),
+    },
+    cli: {
+      log: jest.fn(),
+    },
+  };
+  const optionsMock = {
+    function: 'testFunction',
+  };
+
+  beforeEach(() => {
+    plugin = new ServerlessPluginInvokeLocalEnvironment(serverlessMock, optionsMock);
+  });
+
+  test('it binds to before invoke local', () => {
+    expect(plugin.hooks).toHaveProperty('before:invoke:local:invoke');
+    expect(plugin.hooks['before:invoke:local:invoke']).toBeInstanceOf(Function);
+  });
+
+  test('to return configuration values', () => {
+    expect(plugin.getConfig()).toEqual(configString);
+  });
+
+  test('to set process.env', () => {
+    plugin.overwriteEnvironment();
+
+    expect(process.env).toHaveProperty(configKey);
+    expect(process.env.KEY).toEqual(configValue);
+  });
 });
 
-test('it binds to before invoke local', () => {
-  expect(plugin.hooks).toHaveProperty('before:invoke:local:invoke');
-  expect(plugin.hooks['before:invoke:local:invoke']).toBeInstanceOf(Function);
-});
+describe('no configuration provided', () => {
+  const serverlessMock = {
+    service: {
+      custom: {},
+      getFunction: jest.fn(() => ({
+        environment: {},
+      })),
+    },
+    cli: {
+      log: jest.fn(),
+    },
+  };
+  const optionsMock = {
+    function: 'testFunction',
+  };
 
-test('to return configuration values', () => {
-  expect(plugin.getConfig()).toEqual(configString);
-});
+  beforeEach(() => {
+    plugin = new ServerlessPluginInvokeLocalEnvironment(serverlessMock, optionsMock);
+  });
 
-test('to set process.env', () => {
-  plugin.overwriteEnvironment();
-
-  expect(process.env).toHaveProperty(configKey);
-  expect(process.env.KEY).toEqual(configValue);
+  test('to return empty configuration', () => {
+    expect(plugin.getConfig()).toEqual('');
+  });
 });
